2ab9a0d4c4f2c0282cc09c7be39c93d0ceccdaa8
Simon Cruanes <simon.cruanes.2007@m4x.org>
2021-06-23 22:41:53 -0400

    add basic test

---
+++ tests/run_tests.ml
@@ -0,0 +1,80 @@
+
+open Quip_core.Common
+open OUnit2
+module K = Trustee_core.Kernel
+module Check = Quip_check.Check
+
+module Make() = struct
+  let ctx = K.Ctx.create()
+  module E = (val K.make_expr ctx)
+  module Th = (val K.make_thm ctx)
+
+  let _src = Logs.Src.create "quip.test"
+  module Log = (val Logs.src_log _src)
+
+  let parse_pb_str line s =
+    match
+      Quip_core.Problem_parser.(parse_string ~syn:Smt2) ctx s
+    with
+    | Ok x -> x
+    | Error e -> errorf "cannot parse problem at line %d:@ %s" line e
+
+  let parse_proof_str line s =
+    try Quip_core.Parser.Proof.parse_string s
+    with e -> errorf "cannot parse proof at line %d:@ %a" line Fmt.exn e
+
+  let _debug = try Sys.getenv "DEBUG"="1" with _ -> false
+  let () =
+    Logs.set_reporter (Logs.format_reporter());
+    Logs.set_level ~all:true (Some (if _debug then Logs.Debug else Logs.Warning));
+    Logs.Src.set_level _src (Some (if _debug then Logs.Debug else Logs.Info))
+
+  let test_proof ~expect line pb proof =
+    let pb = parse_pb_str line pb in
+    let proof = parse_proof_str line proof in
+    let checker = Check.create ctx pb in
+    let ok, _stats = Check.check_proof checker proof in
+    Logs.info (fun k->k"line %d: res %b, stats %a@." line ok Check.pp_stats _stats);
+    assert_equal ~msg:(Printf.sprintf "expect %B on line %d" expect line)
+      ~printer:Fmt.(to_string bool) expect ok;
+    ()
+
+  (* a SMT2 prelude *)
+  let prelude0 = {|
+    (declare-sort U 0)
+    (declare-fun a () U)
+    (declare-fun b () U)
+    (declare-fun c () U)
+    (declare-fun f1 (U) U)
+    (declare-fun g1 (U) U)
+    (declare-fun f2 (U U) U)
+    (declare-fun g2 (U U) U)
+    (declare-fun p1 (U) Bool)
+  |}
+
+  let l = [
+    (__LINE__, true, prelude0,
+    {|(quip 1 (steps () (
+        (stepc c0 (cl (+ (p1 a))) (assert-c (cl (+ (p1 a)))))
+        (stepc c1 (cl (- (p1 b))) (assert-c (cl (- (p1 b)))))
+        (stepc c2 (cl (+ (= a b))) (assert-c (cl (+ (= a b)))))
+        (stepc c3 (cl (- (= a b)) (- (p1 a)) (+ (p1 b)))
+          (ccl (cl (- (= a b)) (- (p1 a)) (+ (p1 b)))))
+        (stepc c4 (cl) (hres (@ c3) ((r1 (@ c0)) (r1 (@ c1)) (r1 (@ c2))))))))
+        |});
+  ]
+
+  let suite =
+    let l =
+      List.map
+        (fun (line,expect,pb,proof) ->
+           (Printf.sprintf "line %d" line) >:: fun _ctx ->
+               test_proof ~expect line pb proof) l
+    in
+    "quip" >::: l
+end
+
+
+let () =
+  let module M = Make() in
+  OUnit2.run_test_tt_main M.suite

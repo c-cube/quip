a07ae68c2f668c533c40ddff2cc0eaadf22e45a5
Simon Cruanes <simon.cruanes.2007@m4x.org>
2021-07-11 22:05:41 -0400

    improve cli

--- src/main/quip.ml
+++ src/main/quip.ml
@@ -25,17 +25,23 @@ let setup_log lvl : unit =
 
 (* check proof for problem, then exits *)
 let main ~quiet ~problem proof : 'a =
-  Log.info (fun k->k"process proof '%s' with problem '%s'" proof problem);
+  Log.info (fun k->k"process proof '%s' with problem %a" proof (Fmt.opt Fmt.string_quoted) problem);
   let proof = CCIO.with_in proof (Parser.Proof.parse_chan ~filename:proof) in
   if not quiet then (
-    Log.debug (fun k->k"parsed proof");
-    Fmt.printf "parsed proof:@ %a@." Ast.Proof.pp proof;
+    Log.info (fun k->k"parsed proof");
+    Log.debug (fun k->k"parsed proof:@ %a" Ast.Proof.pp proof);
   );
   let ctx = K.Ctx.create() in
-  let problem = Problem_parser.parse_file_exn ctx problem in
-  Log.debug (fun k->k"parsed problem:@ %a" Env.pp_debug problem);
+  let env = match problem with
+    | None -> Env.make_new_smt2 ~ctx ()
+    | Some pb ->
+      let env = Problem_parser.parse_file_exn ctx pb in
+      Log.info (fun k->k"parsed problem from '%s'" pb);
+      Log.debug (fun k->k"parsed problem:@ %a" Env.pp_debug env);
+      env
+  in
 
-  let checker = Quip_check.Check.create ctx problem in
+  let checker = Quip_check.Check.create ctx env in
 
   Fmt.printf "checking proof…@.";
   let proof_valid, stats = Quip_check.Check.check_proof checker proof in
@@ -51,24 +57,26 @@ let () =
   let files = ref [] in
   let color = ref true in
   let quiet = ref false in
+  let problem = ref None in
 
   let log_level = ref Logs.Warning in
 
   let opts = [
     "--info", Arg.Unit (fun() -> log_level := Logs.Info), " set log level to info";
     "--debug", Arg.Unit (fun() -> log_level := Logs.Debug), " set log level to debug";
+    "--problem", Arg.String (fun s->problem := Some s), " parse context from this problem file";
     "-nc", Arg.Clear color, " disable color";
     "-q", Arg.Set quiet, " quiet mode";
   ] |> Arg.align in
-  Arg.parse opts (fun f -> files := f :: !files) "quip [opt]* problem proof.quip";
+  Arg.parse opts (fun f -> files := f :: !files) "quip [opt]* proof.quip";
 
   setup_log !log_level;
 
   Fmt.set_color_default !color;
   match List.rev !files with
-  | [problem; proof] ->
+  | [proof] ->
     begin
-      try main ~quiet:!quiet ~problem proof
+      try main ~quiet:!quiet ~problem:!problem proof
       with
       | Error msg ->
         Log.err (fun k->k "error: %s" msg);
@@ -78,4 +86,4 @@ let () =
         Log.err (fun k->k "error: %s" msg);
         Fmt.eprintf "@{<Red>Error@}: %s" msg; exit 3
     end
-  | _ -> Fmt.eprintf "expected <problem> <proof>@."; exit 2
+  | _ -> Fmt.eprintf "expected [opt]* <proof>@."; exit 2

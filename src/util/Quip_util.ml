d078cf04b68375063a2f8d5b35183fc668b3dbc5
Simon Cruanes <simon.cruanes.2007@m4x.org>
2021-11-28 13:30:00 -0500

    add quip.util library

---
+++ src/util/Quip_util.ml
@@ -0,0 +1,34 @@
+
+open Quip_core
+open Common
+
+module Logger = Logger
+
+(** Setup a logger at given level *)
+let setup_log lvl : unit =
+  let quip_logger = Logger.create() in
+  Logger.log_to_chan quip_logger stdout;
+  Trustee_core.Log.(
+    let module Log = (val Logs.src_log (Logs.Src.create "trustee")) in
+    set_level 50;
+    set_logger {
+      log=fun lvl k ->
+        let m : _ Logs.msgf = fun logger -> k (fun fmt -> logger ?header:None ?tags:None fmt)in
+        match lvl with
+        | 0 -> Log.app m
+        | 1 -> Log.info m
+        | _ -> Log.debug m
+    });
+  Logs.set_reporter (Logger.as_reporter quip_logger);
+  Logs.set_level ~all:true (Some lvl)
+
+(** Open files, including compressed ones *)
+let with_file_in (file:string) f =
+  if Filename.extension file = ".gz" then (
+    let p = Unix.open_process_in
+        (Printf.sprintf "gzip --keep -d -c \"%s\"" (String.escaped file))
+    in
+    CCFun.finally1
+      ~h:(fun () -> Unix.close_process_in p)
+      f p
+  ) else CCIO.with_in file f

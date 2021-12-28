aaeb366d3231b01cf8ba87e5e6f4fa8481d4b3d2
Simon Cruanes <simon.cruanes.2007@m4x.org>
2021-12-27 23:27:39 -0500

    fix build

--- src/util/Quip_util.ml
+++ src/util/Quip_util.ml
@@ -10,17 +10,6 @@ module Error = Error
 let setup_log lvl : unit =
   let quip_logger = Logger.create() in
   Logger.log_to_chan quip_logger stdout;
-  Trustee_core.Log.(
-    let module Log = (val Logs.src_log (Logs.Src.create "trustee")) in
-    set_level 50;
-    set_logger {
-      log=fun lvl k ->
-        let m : _ Logs.msgf = fun logger -> k (fun fmt -> logger ?header:None ?tags:None fmt)in
-        match lvl with
-        | 0 -> Log.app m
-        | 1 -> Log.info m
-        | _ -> Log.debug m
-    });
   Logs.set_reporter (Logger.as_reporter quip_logger);
   Logs.set_level ~all:true (Some lvl)
 
